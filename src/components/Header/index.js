f63bcd69373f0e31c016e5fe7c36d5b98f973d3c
Broda Noel <brodanoel@gmail.com>
2020-02-23 17:41:00 +0100

    Hide Contact from Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,9 +9,9 @@ const Header = () => {
         <Link to="/">
           <img src="/img/nofilter-logo.png" alt="logo" />
         </Link>
-        <Link to="/contact" className="contact">
+        {/* <Link to="/contact" className="contact">
           Contact us
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
