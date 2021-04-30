00d89d54b96b97b9970108516bb105dc006bd05b
Broda Noel <brodanoel@gmail.com>
2021-04-30 04:04:43 -0300

    Implement French profesional translation

--- src/utils/translations/fr.js
+++ src/utils/translations/fr.js
@@ -2,33 +2,34 @@ export default {
   'App Store Button': 'Bouton App Store',
   'App Store QR Button': "Bouton QR de l'App Store",
   featuresQuality:
-    "Contrairement à d'autres applications, toutes nos photos passent par un <1>contrôle de qualité approfondi</1>. Moins de 1% des photos sont approuvées",
+    "Contrairement à d'autres applications, toutes nos photos sont soumises à un <1>contrôle de qualité approfondi</1>. Moins de 1 % des photos sont approuvées",
   featuresQuantity:
-    'NoFilter contient <1>5000 spots</1> dans le monde et environ <1>100 nouvelles photos sont ajoutées chaque semaine</1>',
-  featuresPrice: "Oh... Ouais... Et c'est <1>gratuit</1>, et disponible pour <1>Android et iOS</1>",
+    'NoFilter rassemble <1>5 000 endroits</1> à travers le monde, et environ <1>100 nouvelles photos sont ajoutées chaque semaine</1>',
+  featuresPrice:
+    "Oh... Ouais... Et c'est <1>gratuit</1> , et disponible pour <1>Android et iOS</1>",
   'Geolocation is not supported by this browser':
     "La géolocalisation n'est pas prise en charge par ce navigateur",
-  'Getting photo locations': 'Obtenir des emplacements de photos',
-  'Getting your location...': 'Obtenir votre position...',
+  'Getting photo locations': 'Trouver des emplacements pour prendre des photos',
+  'Getting your location...': 'Obtention de votre emplacement...',
   'Google Play Button': 'Bouton Google Play',
-  'Google Play QR Button': 'Bouton Google Play QR',
-  'Join NoFilter today': "Rejoignez NoFilter aujourd'hui",
+  'Google Play QR Button': 'Bouton QR de Google Play',
+  'Join NoFilter today': "Rejoignez NoFilter dès aujourd'hui",
   'Our goal': 'Notre objectif',
   ourGoal:
-    'Aidez les <1>photographes et voyageurs</1> qui veulent prendre <1>des photos incroyables</1>, en fournissant une application où ils peuvent voir des emplacements de photos sympas et des destinations de voyage, partout dans le monde.',
-  'Price and Support': 'Prix et assistance',
+    'Aider les <1>photographes et les voyageurs</1> qui veulent réaliser des <1>photos incroyables</1>, en leur fournissant une application qui leur permet de découvrir des endroits sympas où prendre des photos, ainsi que des destinations de voyage, partout dans le monde.',
+  'Price and Support': 'Prix et Assistance',
   Quality: 'Qualité',
   Quantity: 'Quantité',
   'Show me some examples near me!': 'Montrez-moi quelques exemples près de chez moi!',
   'Some examples': 'Quelques exemples',
   titleInstagrammers:
-    'Pour les <1>instagrammeurs</1> qui souhaitent découvrir les meilleurs <1>photoshoot</1> lieux',
+    'Pour les <1>instagrameurs</1> qui veulent découvrir les meilleurs endroits pour leur <1>séance photo</1>',
   titlePhotographers:
-    'Pour les <1>photographes</1> qui souhaitent découvrir les meilleurs spots photo <1>à proximité</1>',
+    'Pour les <1>photographes</1> qui veulent découvrir les meilleurs endroits où prendre des photos <1>à proximité</1>',
   titleTravelers:
-    'Pour les <1>voyageurs</1> qui souhaitent découvrir les meilleurs spots photo <1>dans le monde</1>',
+    'Pour les <1>voyageurs</1> qui veulent découvrir les meilleurs endroits où prendre des photos <1>à travers le monde</1>',
   "Want to see more? Download the app. It's free!":
-    "Vous voulez en voir plus? Téléchargez l'appli. C'est gratuit!",
+    "Envie d'en voir plus? Téléchargez l'application. C'est gratuit!",
   noLocationPermission:
-    'Votre navigateur ne veut pas nous donner votre position :( essayez depuis votre PC, ou téléchargez simplement NoFilter dans votre téléphone et essayez-le pour de vrai!',
+    'Votre navigateur ne veut pas nous donner votre position :( essayez depuis votre PC, ou téléchargez directement NoFilter sur votre téléphone et essayez-le pour de vrai!',
 };
