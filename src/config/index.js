65835b7a4eb5683186e70de3e9acaafb4e93604e
Broda Noel <brodanoel@gmail.com>
2021-05-30 18:31:48 -0300

    Fix endpoint

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const baseUrl =
   window.location.hostname === 'localhost'
     ? 'http://localhost:5000/mari-a5cc7/us-central1'
-    : `https://us-central1-getlobee.cloudfunctions.net`;
+    : `https://us-central1-mari-a5cc7.cloudfunctions.net`;
 
 export default {
   api: {
